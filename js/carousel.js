a8156e3ff98077f3456234c54ab8f9e11cc8b32f
javierbull <javierbullreyes@gmail.com>
2023-02-28 14:38:50 -0500

    carowsel productos index MQ

--- js/carousel.js
+++ js/carousel.js
@@ -9,6 +9,35 @@ $(document).ready(function() {
         autoplayHoverPause: true,
         dots: false,
         nav: true,
+        responsive: {
+            0: {
+              items: 1
+            },
+            360: {
+              items: 1
+            },
+            480: {
+              items: 1
+            },
+            568: {
+              items: 2
+            },
+            740: {
+              items: 2
+            },
+            768: {
+              items: 2
+            },
+            824: {
+              items: 3
+            },
+            1024: {
+              items: 3
+            },
+            1366: {
+              items: 4
+            }
+          }
     });
     // End prueba
 });
