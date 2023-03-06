5612e1f26caaa57849f81d64710ef06df2081a69
javierbull <javierbullreyes@gmail.com>
2023-03-06 11:38:29 -0500

    control quantity js

--- js/main.js
+++ js/main.js
@@ -44,4 +44,24 @@ $('#input-preview-img').change(function(){
 });
 // END LOAD IMG PROFILE
 
+// INCREMENTO - DECREMENTO =================
+
+$('.qtybutton').on('click', function() {
+  var $button = $(this);
+  var oldValue = $button.parent().find("input").val();
+  if ($button.text() == "+") {
+    var newVal = parseFloat(oldValue) + 1;
+  } else {
+    // Don't allow decrementing below zero
+    if (oldValue > 1) {
+      var newVal = parseFloat(oldValue) - 1;
+    } else {
+      newVal = 1;
+    }
+  }
+  $button.parent().find("input").val(newVal);
+});
+
+// FIN INCREMENTO - DECREMENTO
+
 });
